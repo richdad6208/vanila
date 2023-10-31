fb87f48ced1ca3eecba01421c4ebe378de4d5c17
[SW6] 박제성 <richdad6208@gmail.com>
2023-11-01 04:31:40 +0900

    카운터 추가

--- counter/main.js
+++ counter/main.js
@@ -1,6 +1,30 @@
 const $button = document.querySelector(".button-wrapper");
-$button.addEventListener("click", handleClick);
 
 function handleClick(e) {
-  if (e.target.closest(".decrease")) console.log("hi");
+  if (e.target.closest(".decrease")) {
+    count--;
+    printCountOnScreen();
+  }
+  if (e.target.closest(".reset")) {
+    count = 0;
+    printCountOnScreen();
+  }
+  if (e.target.closest(".increase")) {
+    count++;
+    printCountOnScreen();
+  }
 }
+
+const printCountOnScreen = () => {
+  $body.innerHTML = count;
+};
+
+const $body = document.querySelector(".body");
+let count = 0;
+
+const app = () => {
+  printCountOnScreen();
+  $button.addEventListener("click", handleClick);
+};
+
+app();
