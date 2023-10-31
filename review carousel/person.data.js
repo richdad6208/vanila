0af05837e31906e791f271f138bdc718ca9dbbfb
[SW6] 박제성 <richdad6208@gmail.com>
2023-11-01 05:22:17 +0900

    review carousel

---
+++ review carousel/person.data.js
@@ -0,0 +1,50 @@
+export const person = [
+  {
+    id: 1,
+    name: "Audrey B. Therrien",
+    job: "Trauma nurse",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum perspiciatis voluptatem voluptate, nemo fugit temporibus repellat et dolorem repudiandae ratione consectetur nam, porro sint minus saepe sit mollitia reiciendis molestias!",
+    avatarUrl: `https://i.pravatar.cc/150?u=1`,
+  },
+  {
+    id: 2,
+    name: "James S. Chapman",
+    job: "Civil service clerk",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum perspiciatis voluptatem voluptate, nemo fugit temporibus repellat et dolorem repudiandae ratione consectetur nam, porro sint minus saepe sit mollitia reiciendis molestias!",
+    avatarUrl: `https://i.pravatar.cc/150?u=2`,
+  },
+  {
+    id: 3,
+    name: "Susan C. Stocks",
+    job: "Private household cook",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum perspiciatis voluptatem voluptate, nemo fugit temporibus repellat et dolorem repudiandae ratione consectetur nam, porro sint minus saepe sit mollitia reiciendis molestias!",
+    avatarUrl: `https://i.pravatar.cc/150?u=3`,
+  },
+  {
+    id: 4,
+    name: "Cheryl A. Owens",
+    job: "Author",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum perspiciatis voluptatem voluptate, nemo fugit temporibus repellat et dolorem repudiandae ratione consectetur nam, porro sint minus saepe sit mollitia reiciendis molestias!",
+    avatarUrl: `https://i.pravatar.cc/150?u=4`,
+  },
+  {
+    id: 5,
+    name: "Donald K. Kimberlin",
+    job: "Finance officer",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum perspiciatis voluptatem voluptate, nemo fugit temporibus repellat et dolorem repudiandae ratione consectetur nam, porro sint minus saepe sit mollitia reiciendis molestias!",
+    avatarUrl: `https://i.pravatar.cc/150?u=5`,
+  },
+  {
+    id: 6,
+    name: "Michele P. Niemann",
+    job: "Localization translator",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum perspiciatis voluptatem voluptate, nemo fugit temporibus repellat et dolorem repudiandae ratione consectetur nam, porro sint minus saepe sit mollitia reiciendis molestias!",
+    avatarUrl: `https://i.pravatar.cc/150?u=6`,
+  },
+];
